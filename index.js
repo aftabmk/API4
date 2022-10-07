ef01072a3ef3b5cc500bb2f9b02738c3dcc9a616
Aftab mohammed k <87110394+aftabmk@users.noreply.github.com>
2022-10-07 10:38:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
+const ax = require("axios");
+const ex = require("express");
+const cors = require('cors')
 
+const url = 'https://kf8gy9csr0.execute-api.ap-south-1.amazonaws.com/webapi/index/activeStockList'
+
+const path = 'index'
+
+async function f()
+
+{
+	try{
+		const req = await ax.get(url);
+		const data = req.data
+		return data
+		// console.log(data)
+	}
+	catch(e)
+	{
+		console.log(e)
+	}
+
+}
+
+//EXPRESS APP
+
+const app = ex()
+app.use(cors())
+
+const port = process.env.PORT || 5000;
+
+app.get(`/${path}`, async (req, res) => {
+
+	try {
+
+		const api = await f();
+
+		return res.status(200).send(api)
+	}
+	catch (err) {
+		return res.status(500).json({
+			err: err.toString(),
+		})
+	}
+})
+
+app.listen(port, () => {
+	console.log(`running on port http://localhost:5000/${path}`)
+
+})
